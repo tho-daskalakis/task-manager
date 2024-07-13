39a22c601d22c881b97736675ca8ccf7db1a7ca0
tho-daskalakis <108701212+tho-daskalakis@users.noreply.github.com>
2024-07-13 22:41:02 +0300

    Add user_id as task table column

--- database/migrations/2024_07_13_161332_create_tasks_table.php
+++ database/migrations/2024_07_13_161332_create_tasks_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->timestamps();
             $table->string('title');
             $table->string('description');
+            $table->foreignId('user_id');
         });
     }
 
